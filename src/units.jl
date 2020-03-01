bbe34d4d1b1bbb63ce269ca6cb498700a2704ee5
Mandar Chitre <mandar@nus.edu.sg>
2020-03-01 21:11:49 +0800

    style(units): minor cleanup

--- src/units.jl
+++ src/units.jl
@@ -1,9 +1,8 @@
-using Unitful
-
+import Unitful: Quantity, ustrip, upreferred, 𝐓
 import Base: (:)
 
-const TimeQuantity = Quantity{T,Unitful.𝐓,S} where {T,S}
-const FreqQuantity = Quantity{T,Unitful.𝐓^-1,S} where {T,S}
+const TimeQuantity = Quantity{T,𝐓,S} where {T,S}
+const FreqQuantity = Quantity{T,𝐓^-1,S} where {T,S}
 
 timeQ(q) = float(q)
 timeQ(q::TimeQuantity) = float(ustrip(upreferred(q)))
@@ -11,7 +10,7 @@ timeQ(q::TimeQuantity) = float(ustrip(upreferred(q)))
 freqQ(q) = float(q)
 freqQ(q::FreqQuantity) = float(ustrip(upreferred(q)))
 
-Base.getindex(s::AbstractArray, t::Quantity{T,Unitful.𝐓,U}, ndx...) where {T,U} = s[toindex(t),ndx...]
+Base.getindex(s::AbstractArray, t::Quantity{T,𝐓,U}, ndx...) where {T,U} = s[toindex(t),ndx...]
 
 (:)(start::TimeQuantity, stop::TimeQuantity) = toindex(start):toindex(stop)
 (:)(start::Int, stop::TimeQuantity) = start:toindex(stop)
