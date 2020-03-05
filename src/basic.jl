1fda38fcc92ff975f613c2e162102317166c7a3d
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2020-03-05 17:54:07 +0800

    compare singleton types

--- src/basic.jl
+++ src/basic.jl
@@ -118,7 +118,7 @@ function fir(n, f1, f2=nothing; fs=deffs[], method=FIRWindow(hanning(n)))
   fs = freqQ(fs)
   if f1 == 0
     f = Lowpass(freqQ(f2); fs=fs)
-  elseif f2 == nothing || freqQ(f2) == fs/2
+  elseif f2 === nothing || freqQ(f2) == fs/2
     f = Highpass(freqQ(f1); fs=fs)
   else
     f = Bandpass(freqQ(f1), freqQ(f2); fs=fs)
