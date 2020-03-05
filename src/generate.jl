3842cabbe2cb731deeb726fe9c240a2c5f9c4209
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2020-03-05 17:52:39 +0800

    exp(im) to cis and compare singleton types

--- src/generate.jl
+++ src/generate.jl
@@ -4,7 +4,7 @@ export hanning, hamming, tukey, cosine, lanczos, triang, bartlett, gaussian, bar
 "Generate a sinusoidal signal."
 function cw(freq, duration; fs=deffs[], phase=0.0, window=nothing)
     t = 0:1/freqQ(fs):timeQ(duration)
-    x = exp.(2im*π*freqQ(freq).*t .+ phase)
+    x = cis.(2π*freqQ(freq).*t .+ phase)
     if window != nothing
         x .*= getwindow(window, length(t))
     end
@@ -19,14 +19,14 @@ function chirp(freq1, freq2, duration; fs=deffs[], shape=:linear, phase=0.0, win
     t = 0:1/freqQ(fs):d
     if shape == :linear
         cby2 = (f2-f1)/d/2.0
-        x = exp.(2im*π .* (cby2.*t.^2 .+ f1.*t) .+ phase)
+        x = cis.(2π .* (cby2.*t.^2 .+ f1.*t) .+ phase)
     elseif shape == :hyperbolic
         s = f2*d/(f2-f1)
-        x = exp.(-2im*π*s*f1 .* log.(abs.(1.0 .- t./s)))
+        x = cis.(-2π*s*f1 .* log.(abs.(1.0 .- t./s)))
     else
-        @assert false "Unknown chirp shape"
+        throw(ArgumentError("Unknown chirp shape: $shape"))
     end
-    if window != nothing
+    if window !== nothing
         x .*= getwindow(window, length(t))
     end
     return x
@@ -35,7 +35,7 @@ end
 ### utility functions
 
 function getwindow(window, n)
-    window == nothing && return ones(n)
+    window === nothing && return ones(n)
     window isa Tuple && return window[1](n, window[2])
     window(n)
 end
