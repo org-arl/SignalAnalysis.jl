b106d44e633be6b752c5d7a214f2656105bc5e25
Mandar Chitre <mandar@nus.edu.sg>
2020-03-11 00:46:21 +0800

    feat(dsp): support multi-signal DC removal, and add DEMON processing

--- src/dsp.jl
+++ src/dsp.jl
@@ -1,4 +1,4 @@
-export fir, removedc, removedc!
+export fir, removedc, removedc!, demon
 
 "Design FIR filter."
 function fir(n, f1, f2=nothing; fs=deffs[], method=FIRWindow(hanning(n)))
@@ -16,10 +16,12 @@ end
 "DC removal filter."
 function removedc!(s; α=0.95)
   # based on Lyons 2011 (3rd ed) real-time DC removal filter in Fig. 13-62(d)
-  for j = 2:length(s)
-    s[j] += α*s[j-1]
+  for k = 1:size(s,2)
+    for j = 2:size(s,1)
+      s[j,k] += α*s[j-1,k]
+    end
+    s[2:end,k] .+= -s[1:end-1,k]
   end
-  s[2:end] .+= -s[1:end-1]
   s *= sqrt(α)
   return s
 end
@@ -27,3 +29,28 @@ end
 "DC removal filter."
 removedc(s; α=0.95) = removedc!(copy(s); α=α)
 
+"Estimate DEMON spectrum."
+function demon(x; fs=deffs[], downsample=250, method=:rms, cutoff=1.0)
+  local y
+  fs /= downsample
+  for k = 1:size(x,2)
+    if downsample == 1
+      y1 = abs.(hilbert(x[:,k]))
+    elseif method == :rms
+      y1 = sqrt.(mean.(Periodograms.arraysplit(abs2.(hilbert(x[:,k])), downsample, 0)))
+    elseif method == :mean
+      y1 = mean.(Periodograms.arraysplit(abs.(hilbert(x[:,k])), downsample, 0))
+    elseif method == :fir
+      aaf = fir(127, 0, 0.48fs; fs=fs)
+      y1 = filtfilt(aaf, abs.(hilbert(x[:,k])))[1:downsample:end]
+    else
+      throw(ArgumentError("Unknown method"))
+    end
+    if k == 1
+      y = zeros(length(y1), size(x,2))
+    end
+    y[:,k] .= y1
+  end
+  hpf = fir(127, cutoff; fs=fs)
+  filtfilt(hpf, y)
+end
--- src/generate.jl
+++ src/generate.jl
@@ -3,39 +3,39 @@ export hanning, hamming, tukey, cosine, lanczos, triang, bartlett, gaussian, bar
 
 "Generate a sinusoidal signal."
 function cw(freq, duration; fs=deffs[], phase=0.0, window=nothing)
-    t = 0:1/freqQ(fs):timeQ(duration)
-    x = cis.(2π*freqQ(freq).*t .+ phase)
-    if window != nothing
-        x .*= getwindow(window, length(t))
-    end
-    return x
+  t = 0:1/freqQ(fs):timeQ(duration)
+  x = cis.(2π*freqQ(freq).*t .+ phase)
+  if window != nothing
+    x .*= getwindow(window, length(t))
+  end
+  return x
 end
 
 "Generate a frequency modulated chirp signal."
 function chirp(freq1, freq2, duration; fs=deffs[], shape=:linear, phase=0.0, window=nothing)
-    f1 = freqQ(freq1)
-    f2 = freqQ(freq2)
-    d = timeQ(duration)
-    t = 0:1/freqQ(fs):d
-    if shape == :linear
-        cby2 = (f2-f1)/d/2.0
-        x = cis.(2π .* (cby2.*t.^2 .+ f1.*t) .+ phase)
-    elseif shape == :hyperbolic
-        s = f2*d/(f2-f1)
-        x = cis.(-2π*s*f1 .* log.(abs.(1.0 .- t./s)))
-    else
-        throw(ArgumentError("Unknown chirp shape: $shape"))
-    end
-    if window !== nothing
-        x .*= getwindow(window, length(t))
-    end
-    return x
+  f1 = freqQ(freq1)
+  f2 = freqQ(freq2)
+  d = timeQ(duration)
+  t = 0:1/freqQ(fs):d
+  if shape == :linear
+    cby2 = (f2-f1)/d/2.0
+    x = cis.(2π .* (cby2.*t.^2 .+ f1.*t) .+ phase)
+  elseif shape == :hyperbolic
+    s = f2*d/(f2-f1)
+    x = cis.(-2π*s*f1 .* log.(abs.(1.0 .- t./s)))
+  else
+    throw(ArgumentError("Unknown chirp shape: $shape"))
+  end
+  if window !== nothing
+    x .*= getwindow(window, length(t))
+  end
+  return x
 end
 
 ### utility functions
 
 function getwindow(window, n)
-    window === nothing && return ones(n)
-    window isa Tuple && return window[1](n, window[2])
-    window(n)
+  window === nothing && return ones(n)
+  window isa Tuple && return window[1](n, window[2])
+  window(n)
 end
