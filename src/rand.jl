987b6a8121e13699532e52bcf9dea9a1d4cddca4
ymtoo <ymtoo86@gmail.com>
2021-03-11 14:41:29 +0800

    fix: use one(T) and zero(T)

--- src/rand.jl
+++ src/rand.jl
@@ -53,7 +53,7 @@ PinkGaussian(n) = PinkGaussian(n=n)
 function Random.rand!(rng::AbstractRNG, d::RedGaussian{T}, x::AbstractVector{T}) where {T<:Real}
   length(x) >= d.n || throw(ArgumentError("length of x must be at least n"))
   extra = 100
-  v = rand(rng, Normal{T}(convert(T, 0.0), d.σ), length(x)+extra)
+  v = rand(rng, Normal{T}(zero(T), d.σ), length(x)+extra)
   for j = 2:length(v)
     v[j] += v[j-1]
   end
@@ -68,7 +68,7 @@ function Random.rand!(rng::AbstractRNG, d::PinkGaussian{T}, x::AbstractVector{T}
   hb = [0.049922035, -0.095993537, 0.050612699, -0.004408786]
   ha = [1.0, -2.494956002, 2.017265875, -0.522189400]
   extra = 1430
-  v = rand(rng, Normal{T}(convert(T, 0.0), d.σ), length(x)+extra)
+  v = rand(rng, Normal{T}(zero(T), d.σ), length(x)+extra)
   v = filt(hb, ha, v)/0.08680587859687908
   x .= v[extra+1:end]
   return x
