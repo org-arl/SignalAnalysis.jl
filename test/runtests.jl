aede6201b951a114e2f4304fa25ce72707127b55
Mandar Chitre <mandar@nus.edu.sg>
2023-08-28 03:07:57 +0800

    feat(dsp): add mfo option to findsignal()

--- test/runtests.jl
+++ test/runtests.jl
@@ -636,15 +636,17 @@ end
   @test energy(z) ≈ energy(x)
 
   y .+= 0.1 * randn(rng, length(y))
-  t, a = findsignal(x, y, 3; coarse=false)
+  t, a, m = findsignal(x, y, 3; coarse=false)
   @test t ≈ [0.000775, 0.001545, 0.003124] atol=2e-6
   @test real(a) / real(a[1]) ≈ [1.0, -0.8, 0.6] atol=1e-2
-  t, a = findsignal(x, y, 3; coarse=true)
+  @test length(m) == 0
+  t, a, m = findsignal(x, y, 3; coarse=true, mfo=true)
   @test t ≈ [0.000775, 0.001545, 0.003124] atol=1e-5
   @test real(a) / real(a[1]) ≈ [1.0, -0.8, 0.6] atol=1e-2
+  @test length(m) > 0
 
   y = compose(real(x), time([32.75, 64.25, 129.0], x), [0.8, -0.7, 1.0]; duration=0.2)
-  t, a = findsignal(x, y, 3; coarse=false)
+  t, a, m = findsignal(x, y, 3; coarse=false)
   @test t ≈ [0.000775, 0.001545, 0.003124] atol=2e-6
   @test real(a) / real(a[3]) ≈ [0.8, -0.7, 1.0] atol=1e-2
 
