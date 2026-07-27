5375f037dc827bc7dae8403e3b6ebc09daacaf6c
Mandar Chitre <mandar@nus.edu.sg>
2026-07-28 02:14:49 +0800

    refactor(rand): drop Distributions dependency
    Distributions pulls in a large transitive tree (StatsFuns, Rmath, PDMats, ...)
    and libRmath-julia's TLS segment breaks library loading on old-glibc systems
    (JuliaStats/Rmath-julia#56). All we used was the rand()/rand!() fallback
    machinery, now defined locally against a NoiseDistribution abstract type.
    
    BREAKING: RedGaussian/PinkGaussian are no longer
    Distributions.ContinuousMultivariateDistribution subtypes.
    
    Closes #50

--- src/rand.jl
+++ src/rand.jl
@@ -1,9 +1,16 @@
 using Random
-using Distributions
 
 export RedGaussian, PinkGaussian
 
-# TODO: doesn't handle rand(::RedGaussian, m) for m samples of n-vectors yet (see _rand!)
+# We deliberately avoid subtyping Distributions.ContinuousMultivariateDistribution
+# and define our own NoiseDistribution instead: Distributions pulls in a large
+# transitive dependency tree (StatsFuns, Rmath, PDMats, ...), and libRmath-julia's
+# TLS segment breaks library loading on old-glibc systems by exhausting the static
+# TLS surplus (https://github.com/JuliaStats/Rmath-julia/issues/56, see also
+# https://github.com/org-arl/SignalAnalysis.jl/issues/50). All we need from
+# Distributions is the family of rand()/rand!() fallbacks, which we define
+# ourselves below.
+abstract type NoiseDistribution end
 
 """
 $(TYPEDEF)
@@ -20,7 +27,7 @@ julia> rand(RedGaussian(1000, 2.0))
    ⋮
 ```
 """
-Base.@kwdef struct RedGaussian{T} <: Distributions.ContinuousMultivariateDistribution
+Base.@kwdef struct RedGaussian{T} <: NoiseDistribution
   n::Int
   σ::T = 1.0
 end
@@ -42,7 +49,7 @@ julia> rand(PinkGaussian(1000, 2.0))
    ⋮
 ```
 """
-Base.@kwdef struct PinkGaussian{T} <: Distributions.ContinuousMultivariateDistribution
+Base.@kwdef struct PinkGaussian{T} <: NoiseDistribution
   n::Int
   σ::T = 1.0
 end
@@ -52,7 +59,7 @@ PinkGaussian(n) = PinkGaussian(n=n)
 function Random.rand!(rng::AbstractRNG, d::RedGaussian{T}, x::AbstractVector{T}) where {T<:Real}
   length(x) >= d.n || throw(ArgumentError("length of x must be at least n"))
   extra = 100
-  v = rand(rng, Normal{T}(zero(T), d.σ), length(x)+extra)
+  v = d.σ .* randn(rng, T, length(x)+extra)
   for j = 2:length(v)
     v[j] += v[j-1]
   end
@@ -67,14 +74,21 @@ function Random.rand!(rng::AbstractRNG, d::PinkGaussian{T}, x::AbstractVector{T}
   hb = [0.049922035, -0.095993537, 0.050612699, -0.004408786]
   ha = [1.0, -2.494956002, 2.017265875, -0.522189400]
   extra = 1430
-  v = rand(rng, Normal{T}(zero(T), d.σ), length(x)+extra)
+  v = d.σ .* randn(rng, T, length(x)+extra)
   v = filt(hb, ha, v)/0.08680587859687908
   x .= v[extra+1:end]
   return x
 end
 
-Base.length(d::RedGaussian) = d.n
-Base.length(d::PinkGaussian) = d.n
+Base.length(d::NoiseDistribution) = d.n
 
-Base.rand(rng::AbstractRNG, d::RedGaussian) = Random.rand!(rng, d, zeros(typeof(d.σ), d.n))
-Base.rand(rng::AbstractRNG, d::PinkGaussian) = Random.rand!(rng, d, zeros(typeof(d.σ), d.n))
+Base.rand(rng::AbstractRNG, d::NoiseDistribution) = rand!(rng, d, zeros(typeof(d.σ), d.n))
+Base.rand(d::NoiseDistribution) = rand(Random.default_rng(), d)
+Random.rand!(d::NoiseDistribution, x::AbstractVecOrMat) = rand!(Random.default_rng(), d, x)
+Base.rand(rng::AbstractRNG, d::NoiseDistribution, m::Integer) = rand!(rng, d, zeros(typeof(d.σ), d.n, m))
+Base.rand(d::NoiseDistribution, m::Integer) = rand(Random.default_rng(), d, m)
+
+function Random.rand!(rng::AbstractRNG, d::NoiseDistribution, A::AbstractMatrix)
+  foreach(x -> rand!(rng, d, x), eachcol(A))
+  return A
+end
